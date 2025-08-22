675d5a9f676bf0a8c3b56cb80eb54f228e20223f
Alejandro.xmk <alejandro.xmk@gmail.com>
2025-08-22 19:53:58 -0400

    Se agrego mas codigo js para cambiar la caja

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const btn = document.getElementById("btn");
 const prf = document.getElementById("parrafo");
+const caja = document.querySelector(".caja");
 
 const cambiar_texto = () => {
   prf.textContent = "Texto cambiado";
 };
+const cambiar_caja = () => {
+  caja.classList.add("caja_cambiada");
+};
 
 btn.addEventListener("click", cambiar_texto);
+btn.addEventListener("click", cambiar_caja);
